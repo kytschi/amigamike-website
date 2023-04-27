66d1bec97904cd4b5c827b206dc30d82c2d2f2e0
Mike Welsh <dev@kytschi.com>
2023-04-27 19:49:37 +0100

    man building that simple window layout was harder than I thought lol

--- public/website/page.php
+++ public/website/page.php
@@ -1,2 +1,3 @@
 <?php
+header("Content-Type: application/json; charset=utf-8");
 echo json_encode($DUMBDOG->page);
--- public/website/workbench.php
+++ public/website/workbench.php
@@ -16,107 +16,84 @@
             <span id="chipram"><strong>1,957,784</strong> Chip-mem</span>
             <span id="otherram"><strong>7,510,420</strong> other mem</span>
         </header>
-        <main class="window">
+        <main class="window active">
             <div class="window-title">
                 <button class="window-close">&nbsp;</button>
                 <span>Workbench</span>
                 <button class="window-max">&nbsp;</button>
                 <button class="window-index">&nbsp;</button>
             </div>
-            <div class="window-body">
-                <div class="window-content">
-                    <button id="btn-menu">
-                        <span>
-                            <i>
-                                <img src="<?= $DUMBDOG->site->theme_folder; ?>/menu.png">
-                            </i>
-                        </span>
-                        <strong>
-                            System
-                        </strong>
-                    </button>
-                    <div class="window active" style="top: 200px; left: 300px">
-                        <div class="window-title">
-                            <button class="window-close">&nbsp;</button>
-                            <span>Active</span>
-                            <button class="window-max">&nbsp;</button>
-                            <button class="window-index">&nbsp;</button>
-                        </div>
-                        <div class="window-body">
-                            <div class="window-content">
-                            <?php
-                            foreach ($DUMBDOG->menu->header as $item) {
-                                ?>
-                                <button>
-                                    <span>
-                                        <i>
-                                            <img src="<?= $DUMBDOG->site->theme_folder; ?>/<?= str_replace(" ", "-", strtolower($item->name)); ?>.png?t=<?= time(); ?>">
-                                        </i>
-                                    </span>
-                                    <strong><?= ucwords($item->name); ?></strong>
-                                </button>
-                                <?php
-                            }
-                            ?>
+            <section>
+                <div class="window-body">
+                    <div class="window-content">
+                        <button id="btn-menu" data-window="window-system">
+                            <span><i><img src="<?= $DUMBDOG->site->theme_folder; ?>/system.png"></i></span>
+                            <strong>System</strong>
+                        </button>
+                        <div id="window-system" class="window active" style="display: block">
+                            <div class="window-title">
+                                <button class="window-close">&nbsp;</button>
+                                <span>System</span>
+                                <button class="window-max">&nbsp;</button>
+                                <button class="window-index">&nbsp;</button>
                             </div>
-                            <div class="window-vert-scroll">
-                                <div class="scroll-bar"></div>
-                                <button class="scroll-bar-up"><span><i>&nbsp;</i></span></button>
-                                <button class="scroll-bar-down"><span><i>&nbsp;</i></span></button>
-                            </div>
-                        </div>
-                        <div class="window-footer">
-                            <div class="window-footer-border"></div>
-                            <div class="window-horz-scroll">
-                                <div class="scroll-bar"></div>
-                                <button class="scroll-bar-left"><span><i>&nbsp;</i></span></button>
-                                <button class="scroll-bar-right"><span><i>&nbsp;</i></span></button>
-                                <button class="window-scroll-resize"><span><i>&nbsp;</i></span></button>
-                            </div>                            
+                            <section>
+                                <div class="window-body">
+                                    <div class="window-content">
+                                    <?php
+                                    foreach ($DUMBDOG->menu->header as $item) {
+                                        ?>
+                                        <button data-api="<?= $item->url; ?>" data-window="window-<?= str_replace(" ", "-", $item->name); ?>">
+                                            <span><i><img src="<?= $DUMBDOG->site->theme_folder; ?>/folder-close.png?t=<?= time(); ?>"></i></span>
+                                            <strong><?= ucwords($item->name); ?></strong>
+                                        </button>
+                                        <?php
+                                    }
+                                    ?>
+                                    </div>
+                                    <div class="window-footer">
+                                        <div class="window-horz-scroll">
+                                            <div class="scroll-bar"></div>
+                                            <button class="scroll-bar-left"><span><i>&nbsp;</i></span></button>
+                                            <button class="scroll-bar-right"><span><i>&nbsp;</i></span></button>
+                                        </div>                            
+                                    </div>
+                                </div>
+                                <div class="window-vert-scroll">
+                                    <div class="scroll-bar"></div>
+                                    <button class="scroll-bar-up"><span><i>&nbsp;</i></span></button>
+                                    <button class="scroll-bar-down"><span><i>&nbsp;</i></span></button>
+                                    <button class="window-scroll-resize"><span><i>&nbsp;</i></span></button>
+                                </div>
+                            </section>
                         </div>
                     </div>
-
-                    <div class="window" style="top: 200px; left: 800px">
-                        <div class="window-title">
-                            <button class="window-close">&nbsp;</button>
-                            <span>Inactive</span>
-                            <button class="window-max">&nbsp;</button>
-                            <button class="window-index">&nbsp;</button>
-                        </div>
-                        <div class="window-body">
-                            <div class="window-content">
-                            <?php
-                            foreach ($DUMBDOG->menu->header as $item) {
-                                ?>
-                                <button>
-                                    <span>
-                                        <i>
-                                            <img src="<?= $DUMBDOG->site->theme_folder; ?>/<?= str_replace(" ", "-", strtolower($item->name)); ?>.png?t=<?= time(); ?>">
-                                        </i>
-                                    </span>
-                                    <strong><?= ucwords($item->name); ?></strong>
-                                </button>
-                                <?php
-                            }
-                            ?>
-                            </div>
-                            <div class="window-vert-scroll">
-                                <div class="scroll-bar"></div>
-                                <button class="scroll-bar-up"><span><i>&nbsp;</i></span></button>
-                                <button class="scroll-bar-down"><span><i>&nbsp;</i></span></button>
-                            </div>
-                        </div>
-                        <div class="window-footer">
-                            <div class="window-footer-border"></div>
-                            <div class="window-horz-scroll">
-                                <div class="scroll-bar"></div>
-                                <button class="scroll-bar-left"><span><i>&nbsp;</i></span></button>
-                                <button class="scroll-bar-right"><span><i>&nbsp;</i></span></button>
-                                <button class="window-scroll-resize"><span><i>&nbsp;</i></span></button>
-                            </div>
+                    <div class="window-footer">
+                        <div class="window-footer-border"></div>
+                        <div class="window-horz-scroll">
+                            <div class="scroll-bar"></div>
+                            <button class="scroll-bar-left"><span><i>&nbsp;</i></span></button>
+                            <button class="scroll-bar-right"><span><i>&nbsp;</i></span></button>                    
                         </div>
                     </div>
                 </div>
+                <div class="window-vert-scroll">
+                    <div class="scroll-bar"></div>
+                    <button class="scroll-bar-up"><span><i>&nbsp;</i></span></button>
+                    <button class="scroll-bar-down"><span><i>&nbsp;</i></span></button>
+                    <button class="window-scroll-resize"><span><i>&nbsp;</i></span></button>
+                </div>
+            </section>
+        </main>
+        <div id="window-template" class="window" style="display: none">
+            <div class="window-title">
+                <button class="window-close">&nbsp;</button>
+                <span>System</span>
+                <button class="window-max">&nbsp;</button>
+                <button class="window-index">&nbsp;</button>
+            </div>
+            <div class="window-body">
+                <div class="window-content"></div>
                 <div class="window-vert-scroll">
                     <div class="scroll-bar"></div>
                     <button class="scroll-bar-up"><span><i>&nbsp;</i></span></button>
@@ -130,25 +107,82 @@
                     <button class="scroll-bar-left"><span><i>&nbsp;</i></span></button>
                     <button class="scroll-bar-right"><span><i>&nbsp;</i></span></button>
                     <button class="window-scroll-resize"><span><i>&nbsp;</i></span></button>
-                </div>
+                </div>                            
             </div>
-        </main>
+        </div>
     </body>
     <script type="text/javascript">
-    $(function() {
-        var top_index = 4;
-        $(".window").draggable({
-            handle: ".window-title"
-        });
-        $(".window-index").click(function () {
-            var div = $(this).parent().parent();
-            if (div.css("z-index") != top_index) {
-                div.css("z-index", parseInt(div.css("z-index")) + 1);
-            } else {
-                top_index = div.css("z-index");
+        var windows = [
+            "window-system"
+        ];
+            
+        function setBinds() {
+            $(".window-close").bind("click", function() {
+                $(this).parent().parent().hide();
+                windowToTop($("main"));
+            });
+            $(".window-index").bind("click", function () {
+                windowToTop($(this).parent().parent());
+            });
+            $(".window").draggable({
+                handle: ".window-title"
+            });
+            $(".scroll-bar-down").bind("click", function () {
+                console.log($(this).parent().parent().children(".window-content"));
+                $(this).parent().parent().children(".window-content").scrollTop({top:"+=5px"}, 800);
+            });
+            $(".scroll-bar-up").bind("click", function () {
+                console.log($(this).parent().parent().children(".window-content"));
+                $(this).parent().parent().children(".window-content").scrollTop({top:"-=5px"}, 800);
+            });
+        }
+
+        function windowToTop(target) {
+            var top_index = (windows.length + 4);
+            $(".window").css("z-index", top_index - 1).removeClass("active");
+            if (target.css("z-index") != top_index) {
+                target.css("z-index", parseInt(target.css("z-index")) + 1);
             }
-            console.log(div.css("z-index"));
+            target.addClass("active");
+        }
+
+        $(function() {
+            setBinds();
+            
+            $("button[data-window]").dblclick(function() {
+                $(this).addClass("open");
+                var window = "#" + $(this).data("window");
+                var startx = $(this).offset().left + 80 + "px";
+                var starty = $(this).offset().top + "px";
+
+                if ($(window).length) {
+                    windowToTop($(window));
+                    $(window).css("top", startx);
+                    $(window).css("left", starty);
+                    $(window).show();
+                } else {
+                    $.getJSON(
+                        $(this).data("api"),
+                        function(data) {
+                            $("#window-template .window-title span").html(data.name);
+                            $("#window-template .window-content").html(data.content);
+                            $("#window-template").addClass("shell");
+
+                            var window = $("#window-template").clone();
+                            window.attr("id", "window-" + String(data.name).toLowerCase().replace(" ", "-"));
+                            windows.push(window.attr("id"));
+
+                            $("main").append(window);
+                            setBinds();
+                            windowToTop(window);
+
+                            window.css("top", startx);
+                            window.css("left", starty);
+                            window.show();
+                        }
+                    );
+                }
+            });
         });
-    });
     </script>
 </html>
